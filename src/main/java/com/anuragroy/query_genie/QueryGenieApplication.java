93c28816d38c34424a5b3592a00c8ecfddfc844e
Anurag Roy <anuragroy17@gmail.com>
2025-04-13 22:28:13 +0530

    📦 NEW: initial commit

---
+++ src/main/java/com/anuragroy/query_genie/QueryGenieApplication.java
@@ -0,0 +1,13 @@
+package com.anuragroy.query_genie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryGenieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryGenieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anuragroy/query_genie/QueryGenieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuragroy.query_genie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryGenieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
